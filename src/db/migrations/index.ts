2eff7ca02d6113d641e031bea20e60bebfac93a8
Gilbert Mak <gilbertmakzr@gmail.com>
2026-06-26 20:25:01 +0800

    Epic 3 add explainability ledger

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,5 +1,6 @@
 import { migration0001 } from "./0001_initial";
 import { migration0002 } from "./0002_reconciliation_trust";
 import { migration0003 } from "./0003_merchant_review_loop";
+import { migration0004 } from "./0004_decision_traces";
 
-export const migrations = [migration0001, migration0002, migration0003] as const;
+export const migrations = [migration0001, migration0002, migration0003, migration0004] as const;
